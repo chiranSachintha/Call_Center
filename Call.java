e9940dc8982e8f46ccfd5f1751e02b5463ec5865
NipunPC <chiransachintha1994@gmail.com>
2019-04-28 18:11:47 +0530

    add getters and setters

--- Call.java
+++ Call.java
@@ -1,2 +1,19 @@
 public class Call {
+
+    private int rank = 0;
+    private String caller;
+    private Employee handler;
+
+    public Call(String caller){
+        this.caller = caller;
+    }
+    public void reply(String message){
+
+    }
+    public void disconnect(){
+
+    }
+    public int getRank(){
+        return this.rank;
+    }
 }
